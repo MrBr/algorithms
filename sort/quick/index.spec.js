cd61fb658ee38f41f2ac2d3a8cab7303423571dc
Luka Bracanovic <lbracano@gmail.com>
2022-01-04 21:05:49 +0100

    Add more sort tests

--- sort/quick/index.spec.js
+++ sort/quick/index.spec.js
@@ -9,4 +9,20 @@ describe('quick sort', () => {
 
     expect(sortedList).toEqual([0,1,2,3,5,6]);
   });
+  test('sorts the array with multiple same values', () => {
+    const unsortedList = [1,0,5,6,0,3,2,3]
+
+    const comparator = (left, right) => left - right;
+    const sortedList = quickSort(unsortedList, comparator);
+
+    expect(sortedList).toEqual([0,0,1,2,3,3,5,6]);
+  });
+  test('sorts already sorted array', () => {
+    const unsortedList = [0,1,2,3,5,6];
+
+    const comparator = (left, right) => left - right;
+    const sortedList = quickSort(unsortedList, comparator);
+
+    expect(sortedList).toEqual([0,1,2,3,5,6]);
+  });
 });
